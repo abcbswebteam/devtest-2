43664a9a2c80e41a2fa8f3f4c44a321eb8943d4c
sandeep-sinha <50221371+sandeep-sinha@users.noreply.github.com>
2019-05-06 22:55:22 -0400

    Update HomeController.vb
    Try to use EagerLoading

--- DevTest2/DevTest2/Controllers/HomeController.vb
+++ DevTest2/DevTest2/Controllers/HomeController.vb
@@ -14,26 +14,35 @@
         Using db As New AppDbContext()
 
             'get customers
-            Dim customers = From c In db.Customers
+        'Instead of doing Lazy Loading, we will do Eager Loading where we will get Customer and related Order data in one db call.
+        'We can loop through for each customer, but now all the data are in system memory so we can avoid db call.
+        Dim customerOrder = From c In db.Customers
+                            join o in db.Orders on o.CustomerID = c.CustomerID
                             Order By c.CustomerId
                             Select c
+            'Declaring a variable to store the previouscustomerID to avoid the loop for same Id
+            Dim previousCustomerID As Integer
 
-            For Each c In customers
+            For Each c In customerOrder
+            'Checking if it is a different customerid then only go further
+            If previousCustomerID <> c.CustomerID Then
                 Response.Write(String.Format("{0}, {1}, ", c.CustomerId, c.Name))
 
                 'get orders
-                Dim orders = From o In db.Orders
-                             Where o.Customer.CustomerId = c.CustomerId
-                             Select o
+                'get all the order for a particular customer
+                Dim orders = customers.Where(Function(x) x.CustomerId = cutomerID).ToList()
 
-                Dim orderCount As Integer = 0
+                Dim orderCount As Integer = 0  
                 Dim salesTotal As Decimal = 0
                 For Each o In orders
                     orderCount += 1
                     salesTotal += o.SalesTotal
                 Next
-
                 Response.Write(String.Format("{0}, {1}", orderCount, salesTotal.ToString("0.##")) & vbCr)
+                'assigning the customerid to variable
+                previousCustomerID = customerID
+                
+                EndIF
             Next
 
         End Using
