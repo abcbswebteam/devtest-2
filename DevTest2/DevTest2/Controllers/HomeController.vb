263d6d72c2b08e0353797c6ded2b1051899309b1
Pravin Nerkar <pravin.nerkar@gmail.com>
2019-06-20 16:17:37 -0500

    Used Left out join with LINQ queries to fetch data . Wrote response at once only instead of doing multiple times

--- DevTest2/DevTest2/Controllers/HomeController.vb
+++ DevTest2/DevTest2/Controllers/HomeController.vb
@@ -1,4 +1,5 @@
-﻿Public Class HomeController
+﻿Imports System.Linq
+Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
@@ -13,28 +14,31 @@
 
         Using db As New AppDbContext()
 
-            'get customers
-            Dim customers = From c In db.Customers
-                            Order By c.CustomerId
-                            Select c
+            Dim result = From c In db.Customers
+                         From o In db.Orders.Where(Function(o) o.Customer.CustomerId = c.CustomerId).DefaultIfEmpty()
+                         Order By c.CustomerId
+                         Select New MyModel With {.CustomerID = c.CustomerId,
+                                                  .Name = c.Name,
+                                                  .OrderID = o.OrderId,
+                                                  .TotalSale = o.SalesTotal
+                                                 }
 
-            For Each c In customers
-                Response.Write(String.Format("{0}, {1}, ", c.CustomerId, c.Name))
 
-                'get orders
-                Dim orders = From o In db.Orders
-                             Where o.Customer.CustomerId = c.CustomerId
-                             Select o
+            Dim summaryResult = From cust In result
+                                Group cust By keys = New With {Key cust.CustomerID, Key cust.Name}
+                                Into Group
+                                Select New With
+                                {
+                                      keys.CustomerID, keys.Name,
+                                     .TotalSales = Group.Sum(Function(x) x.TotalSale),
+                                     .OrderCount = Group.Count()
+                                }
 
-                Dim orderCount As Integer = 0
-                Dim salesTotal As Decimal = 0
-                For Each o In orders
-                    orderCount += 1
-                    salesTotal += o.SalesTotal
-                Next
-
-                Response.Write(String.Format("{0}, {1}", orderCount, salesTotal.ToString("0.##")) & vbCr)
+            For Each item In summaryResult
+                Console.WriteLine(item)
+                Response.Write(String.Format("{0}, {1},{2},{3} ", item.CustomerID, item.Name, item.OrderCount, item.TotalSales.ToString()) & vbCr)
             Next
+
 
         End Using
 
@@ -44,3 +48,17 @@
     End Function
 
 End Class
+
+Public Class MyModel
+
+    Public Property CustomerID As String
+    Public Property Name As String
+    'Public Property OrderID As Integer?
+
+    Public Property OrderID As Integer?
+
+    Public Property TotalSale As Decimal?
+    'Public Property OrderDate As Date?
+
+End Class
+
